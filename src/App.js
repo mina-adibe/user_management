6ff66da70290e6f22c3fd9b744305942bec225d1
mina bessada <48674093+mina-adibe@users.noreply.github.com>
2021-02-27 06:46:37 +0200

    add routs

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
 import "./App.css";
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
+
+import ListOfUsers from "./component/listOfUsers/listOfUsers";
+import ModifyUses from "./component/ModifyUser/modifyUses";
+import LandingPage from "./pages/landingPage";
 
 function App() {
-  return <div></div>;
+  return (
+    <Router>
+      <LandingPage />
+
+      <Switch>
+        <Route path="/ListOfUsers">
+          <ListOfUsers />
+        </Route>
+        <Route path="/ModifyUses">
+          <ModifyUses />
+        </Route>
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
